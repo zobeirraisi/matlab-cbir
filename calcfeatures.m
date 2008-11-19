d2a8e006b2a7f46d58fd1e737e8a9a22f72417b2
nathanj439 <nathanj439@e1684786-b02b-11dd-9253-9d056a18d4a0>
2008-11-19 20:48:09 +0000

    Added the EHD measure.
    I gave it twice as much weight as the other distances.

--- calcfeatures.m
+++ calcfeatures.m
@@ -1,4 +1,4 @@
-function [ hist32 hist128 dom ] = calcfeatures( img )
+function [ hist32 hist128 dom edge ] = calcfeatures( img )
 % Computes the features of the given image.
 
 [hmmd, map] = rgb2quanthmmd(img, 32);
@@ -8,5 +8,7 @@ hist32 = colordescriptor(hmmd, map);
 hist128 = colordescriptor(hmmd, map);
     
 dom = domcolor(img);
+
+edge = ehd(img, 0.10);
 
 end
