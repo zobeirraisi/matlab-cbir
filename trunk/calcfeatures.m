f5c97c8fed93a2396a6a2e7d364f4f90dd64c60d
nathanj439 <nathanj439@e1684786-b02b-11dd-9253-9d056a18d4a0>
2008-11-19 20:50:57 +0000

    Downscale the image by two when calculating the CSD.
    The results do not seem to change much because the dominant colors still
    have the same proportion in the smaller image.

--- trunk/calcfeatures.m
+++ trunk/calcfeatures.m
@@ -6,8 +6,11 @@ hist32 = colordescriptor(hmmd, map);
 
 [hmmd, map] = rgb2quanthmmd(img, 128);
 hist128 = colordescriptor(hmmd, map);
-    
-dom = domcolor(img);
+
+% downscale the img by 2 for calculating csd since it only slightly affects
+% the results and is much faster.
+[x y z] = size(img);
+dom = domcolor(img(1:2:x, 1:2:y, :));
 
 edge = ehd(img, 0.10);
 
